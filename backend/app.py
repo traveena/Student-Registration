a8930c0cf817a7853e6898422bf0e0c523513729
Traveena <traveenachandrasegar@gmail.com>
2022-07-14 15:21:30 +0530

    Backend

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from flask import Flask, jsonify, request
+from models import Base, engine
+from db_conn import DBConn
+
+app = Flask(__name__)
+
+
+# Student CRUD
+@app.route('/students', methods=['GET'])
+def students():
+    data = db.student_view()
+    return {"data": data}
+
+
+@app.route('/student', methods=['POST', 'PUT', 'DELETE'])
+def student():
+    if request.method == 'POST':
+        db.student_insert(request.form)
+
+    elif request.method == 'PUT':
+        db.student_update(request.args)
+
+    elif request.method == 'DELETE':
+        db.student_delete(request.args)
+
+    return jsonify({'data': "student success"})
+
+
+# Course CRUD
+@app.route('/courses', methods=['GET'])
+def courses():
+    data = db.course_view()
+    return {"data": data}
+
+
+@app.route('/course', methods=['PUT', 'POST', 'DELETE'])
+def course():
+    if request.method == 'POST':
+        db.course_insert(request.form)
+
+    elif request.method == 'PUT':
+        db.course_update(request.args)
+
+    elif request.method == 'DELETE':
+        db.course_delete(request.args)
+
+    return jsonify({'data': "course success"})
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    db = DBConn(engine)
+    app.run(debug=True)
---
+++ backend/db_conn.py
@@ -0,0 +1,98 @@
+from models import Student, Course
+import sqlalchemy
+
+
+class DBConn:
+    def __init__(self, engine):
+        session_maker = sqlalchemy.orm.sessionmaker()
+        session_maker.configure(bind=engine)
+        self.session = session_maker()
+
+# Student table connection
+    def student_view(self):
+        students = self.session.query(Student).all()
+        studs = [
+            {
+                "fname": student.fname,
+                "lname": student.lname,
+                "email": student.email,
+                "coursename": student.coursename,
+                "contactno": student.contactno,
+                "dob": student.dob
+            } for student in students]
+        return studs
+
+    def student_insert(self, data):
+        new_student = Student(
+            fname=data['firstname'],
+            lname=data['lastname'],
+            email=data['email'],
+            coursename=data['course'],
+            contactno=data['contactno'],
+            dob=data['dob']
+        )
+        self.session.add(new_student)
+        self.session.commit()
+        return
+
+    def student_update(self, argus):
+        id = argus['sid']
+        data = {
+            "fname": argus['firstname'],
+            "lname": argus['lastname'],
+            "contactno": argus['contactno'],
+            "email": argus['email'],
+            "dob": argus['dob'],
+            "coursename": argus['course']
+        }
+
+        self.session.query(Student).filter(Student.sid == id).update(data)
+        self.session.commit()
+        return
+
+    def student_delete(self, args):
+        id = args['sid']
+
+        self.session.query(Student).filter(Student.sid == id).delete()
+        self.session.commit()
+        return
+
+# Course table connection
+    def course_view(self):
+        courses = self.session.query(Course).all()
+        cour = [
+            {
+                "coursename": course.coursename,
+                "description": course.description,
+                "duration": course.duration
+            } for course in courses]
+        return cour
+
+    def course_insert(self, data):
+        new_course = Course(
+            coursename=data['coursename'],
+            description=data['description'],
+            duration=data['duration']
+        )
+        self.session.add(new_course)
+        self.session.commit()
+        return
+
+    def course_update(self, argus):
+        id = argus['cid']
+        data = {
+            "coursename": argus['coursename'],
+            "description": argus['description'],
+            "duration": argus['duration']
+        }
+
+        self.session.query(Course).filter(Course.cid == id).update(data)
+        self.session.commit()
+        return
+
+    def course_delete(self, args):
+        id = args['cid']
+
+        self.session.query(Course).filter(Course.cid == id).delete()
+        self.session.commit()
+        return
